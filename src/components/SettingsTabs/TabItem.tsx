278e0253ffba252ff60f9ee205a368a9e2fc04fb
Matheus Palmieri <matheuspalmireidev@gmail.com>
2025-10-14 12:24:49 -0300

    feat: focus animation

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -13,9 +13,11 @@ export const TabItem = ({ value, title, isSelected }: TabItemProps) => {
   return (
     <Tabs.Trigger
       value={value}
-      className="relative px-3 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 data-[state=active]:text-violet-600"
+      className="group relative px-3 pb-4 text-sm font-medium text-zinc-500 outline-none hover:text-violet-700 data-[state=active]:text-violet-600"
     >
-      <span>{title}</span>
+      <span className="rounded-xs group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">
+        {title}
+      </span>
 
       {isSelected && (
         <motion.div
--- src/components/form/Textarea.tsx
+++ src/components/form/Textarea.tsx
@@ -3,7 +3,7 @@ import type { ComponentProps } from "react";
 export const Textarea = (props: ComponentProps<"textarea">) => {
   return (
     <textarea
-      className="min-h-30 w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      className="min-h-30 w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 shadow-sm transition-colors duration-200 outline-none focus:border-violet-300"
       {...props}
     />
   );
